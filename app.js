0f167ae20afe86c24f2d1a2609434533b97abbb0
Skylar Broad <skylar.broad@gmail.com>
2020-05-01 13:23:11 -0700

    add the authorization stuff

--- app.js
+++ app.js
@@ -9,4 +9,42 @@ app.get('/api', (req, res) => {
     })
 })
 
+app.post('/api/posts', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if (err) {
+            res.sendStatus(403)
+        } else {
+            res.json({
+                message: 'Post created...',
+                authData
+            })        
+        }
+    })
+})
+
+app.post('/api/login', (req, res) => {
+    // Mock user db auth stuff
+    const user = {
+        ud: 1,
+        username: 'skytos'
+    }
+
+    jwt.sign({user: user}, 'secretkey', {expiresIn: '30s'}, (err, token) => {
+        res.json({token})
+    })
+})
+
+function verifyToken(req, res, next) {
+    // Get auth header
+    // looks like Authorization: Bearer <access_toke>
+    const bearerHeader = req.headers.authorization
+    const bearerToken = bearerHeader && bearerHeader.split(' ')[1]
+    if (bearerToken) {
+        req.token = bearerToken
+        next()
+    } else {
+        res.sendStatus(403)
+    }
+}
+
 app.listen(5000, () => console.log('Server started on port 5000'))
